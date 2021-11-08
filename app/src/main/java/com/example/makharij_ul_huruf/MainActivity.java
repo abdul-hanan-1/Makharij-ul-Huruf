6d9af4c6bb1e7cca217ece6debd76aa1af93ff12
abdul-hanan-1 <a.hanan.rana@gmail.com>
2021-11-09 01:32:58 +0500

    completed first activity

--- app/src/main/java/com/example/makharij_ul_huruf/MainActivity.java
+++ app/src/main/java/com/example/makharij_ul_huruf/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.makharij_ul_huruf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
-
+    private Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.repolink);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://github.com/abdul-hanan-1/Makharij-ul-Huruf.git");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
+
 }
