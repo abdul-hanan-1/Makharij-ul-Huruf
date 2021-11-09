4e6fa9f1afe9f585d492032c86c7a7b9b1353ac0
abdul-hanan-1 <a.hanan.rana@gmail.com>
2021-11-10 00:06:59 +0500

    added java code for result functionality

--- app/src/main/java/com/example/makharij_ul_huruf/result.java
+++ app/src/main/java/com/example/makharij_ul_huruf/result.java
@@ -1,14 +1,47 @@
 package com.example.makharij_ul_huruf;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class result extends AppCompatActivity {
+    TextView tv, tv2, tv3;
+    Button btnRestart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        tv = (TextView)findViewById(R.id.correct);
+        tv2 = (TextView)findViewById(R.id.wrong);
+        tv3 = (TextView)findViewById(R.id.finalscore);
+        btnRestart = (Button) findViewById(R.id.btnRestart);
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("Correct answers: " + quiz.correct + "\n");
+        StringBuffer sb2 = new StringBuffer();
+        sb2.append("Wrong Answers: " + quiz.wrong + "\n");
+        StringBuffer sb3 = new StringBuffer();
+        sb3.append("Final Score: " + quiz.correct + "\n");
+        tv.setText(sb);
+        tv2.setText(sb2);
+        tv3.setText(sb3);
+
+        quiz.correct=0;
+        quiz.wrong=0;
+
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(),quiz.class);
+                startActivity(in);
+            }
+        });
     }
+
 }
