cddd7b340ee6b34873bb4c8f0d63fd9f0ccf89c2
abdul-hanan-1 <a.hanan.rana@gmail.com>
2021-11-11 00:27:01 +0500

    added types of emission points

--- app/src/main/java/com/example/makharij_ul_huruf/MainActivity.java
+++ app/src/main/java/com/example/makharij_ul_huruf/MainActivity.java
@@ -1,21 +1,20 @@
 package com.example.makharij_ul_huruf;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
-    private Button button1,button2,button3;
+    private Button button1,button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1 = findViewById(R.id.repolink);
-        button2 = findViewById(R.id.learn);
-        button3 = findViewById(R.id.quiz);
+        button2 = findViewById(R.id.home);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,24 +26,14 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openLearn();
+                openHome();
             }
         });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openQuiz();
-            }
-        });
-    }
-    public void openLearn(){
-        Intent intent = new Intent(this, learn.class);
-        startActivity(intent);
-    }
-    public void openQuiz(){
-        Intent intent = new Intent(this, quiz.class);
-        startActivity(intent);
-    }
 
+    }
+    public void openHome(){
+        Intent intent = new Intent(this, home.class);
+        startActivity(intent);
+    }
 
 }
