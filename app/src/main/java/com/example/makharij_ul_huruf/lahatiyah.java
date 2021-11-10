ed458ff29bb6dba69053ae2ca761c85643759ece
abdul-hanan-1 <a.hanan.rana@gmail.com>
2021-11-11 03:39:05 +0500

    added lahatiyah activity

---
+++ app/src/main/java/com/example/makharij_ul_huruf/lahatiyah.java
@@ -0,0 +1,14 @@
+package com.example.makharij_ul_huruf;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class lahatiyah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lahatiyah);
+    }
+}
