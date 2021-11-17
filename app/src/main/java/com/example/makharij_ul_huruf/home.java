a4143c61a8c807b0dc297e226417433f7a54e171
abdul-hanan-1 <a.hanan.rana@gmail.com>
2021-11-17 23:59:20 +0500

    added dialog box in learn

--- app/src/main/java/com/example/makharij_ul_huruf/home.java
+++ app/src/main/java/com/example/makharij_ul_huruf/home.java
@@ -1,10 +1,12 @@
 package com.example.makharij_ul_huruf;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class home extends AppCompatActivity {
@@ -33,7 +35,20 @@ public class home extends AppCompatActivity {
         startActivity(intent);
     }
     public void openQuiz(){
-        Intent intent = new Intent(this, quiz.class);
-        startActivity(intent);
+        AlertDialog.Builder builder = new AlertDialog.Builder(home.this);
+        builder.setMessage("The quiz contains 10 questions and you have 5 minutes to complete it.");
+        builder.setTitle("Start Quiz");
+        builder.setCancelable(false);
+        builder.setPositiveButton(
+                "Kill",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int asdf)
+                    {
+                        Intent intent = new Intent(home.this, quiz.class);
+                        startActivity(intent);
+                    }
+                });
+
     }
 }
